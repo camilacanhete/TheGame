ee6fbef417b2ae001c2921c5db95cb2b62c13463
csantos <camilarochadossantos@MacBook-Pro-de-Camila.local>
2017-10-25 22:43:30 -0200

    Latest fixes

--- cordova/platforms/ios/www/controllers/player.js
+++ cordova/platforms/ios/www/controllers/player.js
@@ -70,6 +70,7 @@ PlayerController.prototype.cacheDOM = function() {
 	//this.$btnForwardToNext = $("#forwardToNext");
 	this.$modalInnerWorld = $("#modalInnerWorld");
 	this.$meditationTitle = $("#meditation-title");
+	this.$modalInnerWorldTitle = $("#modalInnerWorldTitle");
 	
 	this.playerContainer = "#cp_container_1";
 };
@@ -260,12 +261,13 @@ PlayerController.prototype.toggleOptions = function() {
 		    });
 			
 			if(unlockedMeditations > 0) {
-				if(unlockedMeditations === 1) {
-					this.$meditationTitle.html(currentMeditation.title);
+				if(currentMeditation.gift) {
+					this.$modalInnerWorldTitle.html("You have unlocked a GIFT!");
 				} else {
-					this.$meditationTitle.html(unlockedMeditations + " new source codes!");
+					this.$modalInnerWorldTitle.html("New Source Code!");
 				}
-
+				
+				this.$meditationTitle.html(currentMeditation.title);
 				this.$modalInnerWorld.modal("show");
 			}
 			
